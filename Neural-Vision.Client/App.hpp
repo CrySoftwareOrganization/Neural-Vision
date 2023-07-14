dfaf7c8ee4f97a41746f57e7bed3a5e275302077
Andrii Volynets <volinec449@gmail.com>
2023-07-14 13:20:29 +0300

    init client

---
+++ Neural-Vision.Client/App.hpp
@@ -0,0 +1,6 @@
+namespace App
+{
+    void App()
+    {
+    }
+}
---
+++ Neural-Vision.Client/index.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "App.hpp"
+
+int main(int argc, char const *argv[])
+{
+    App::App();
+    return 0;
+}
